ce7337c1354f8e2ab931892636c7063add9fd905
Dionara-P <dionara.romenia@gmail.com>
2025-07-19 16:14:14 -0300

    Adicionando testes de transferencia

---
+++ test/tranferencia.test.js
@@ -0,0 +1,59 @@
+const request = require('supertest');
+const { expect } = require('chai')
+
+describe('Transferencias', () => {
+    describe('POST/transferencias', () => {
+       it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou maior que 10.00', async () => {
+            //Capturar o token 
+            const respostaLogin = await request ('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+
+            const token = respostaLogin.body.token
+
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type','application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .send({
+                     contaOrigem: 2,
+                     contaDestino: 1,
+                     valor: 10.01,
+                     token: "" 
+                })
+            expect(resposta.status).to.equal(201);
+
+            console.log(resposta.body)
+
+       })
+       
+       it('Deve retornar falha com 422 quando o valor da transferencia for menor que 10.00', async () => {
+            //Capturar o token 
+            const respostaLogin = await request ('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+
+            const token = respostaLogin.body.token
+
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type','application/json')
+                .set('Authorization', 'Bearer ' + token)
+                .send({
+                     contaOrigem: 2,
+                     contaDestino: 1,
+                     valor: 9.99,
+                     token: "" 
+                })
+            expect(resposta.status).to.equal(422);
+       }) 
+    })
+})
